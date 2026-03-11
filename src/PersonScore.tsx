d2e3a3c059e683ffbf308854edc247b63f472e8f
Mukesh <ganatram@gmail.com>
2026-03-11 09:09:27 +0530

    updatedstatehooks1

--- src/PersonScore.tsx
+++ src/PersonScore.tsx
@@ -1,6 +1,40 @@
-import { useEffect, useState, useRef, useMemo, useCallback } from 'react';
+/* eslint-disable prettier/prettier */
+import { useEffect, useReducer, useRef, useMemo } from 'react';
 import { getPerson } from './getPerson';
-import { Reset } from './Reset';
+// import { Reset } from './Reset';
+
+type State = {
+  name: string | undefined;
+  score: number;
+  loading: boolean;
+};
+
+type Action =
+  | { type: 'initialize'; name: string }
+  | {
+      type: 'increment';
+    }
+  | {
+      type: 'decrement';
+    }
+  | {
+      type: 'reset';
+    };
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'initialize':
+      return { name: action.name, score: 0, loading: false };
+    case 'increment':
+      return { ...state, score: state.score + 1 };
+    case 'decrement':
+      return { ...state, score: state.score - 1 };
+    case 'reset':
+      return { ...state, score: 0 };
+    default:
+      return state;
+  }
+}
 
 function sillyExpensiveFunction() {
   console.log('Executing silly expensive function');
@@ -17,24 +51,36 @@ function sillyExpensiveFunction() {
 export function PersonScore() {
   // const [state,setState]= useState(inititalValue)
 
-  //declaring states
-  const [name, setName] = useState<string | undefined>();
+  //declaring states using useState() hook
+  /* const [name, setName] = useState<string | undefined>();
   const [score, setScore] = useState(0);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true); */
+
+  // declaring states with useReducer() hook
+
+  const [{ name, score, loading }, dispatch] = useReducer(reducer, {
+    name: undefined,
+    score: 0,
+    loading: true,
+  });
+
+  useEffect(() => {
+    getPerson().then(({ name }) => dispatch({ type: 'initialize', name }));
+  }, []);
 
   // use Ref() hook
   const addButtonRef = useRef<HTMLButtonElement>(null); //
 
   // function cpuIntensiveFunction(){}
 
-  useEffect(() => {
+  /*   useEffect(() => {
     getPerson().then((person) => {
       setLoading(false); // modify the loading state -- false
       setName(person.name); // modify the name state  -- 'Bob'
       //console.log('State Values are', loading, name); // state changes are not immediate -- batched before UI render
       //console.log(person.name); //'bob'
     });
-  }, []);
+  }, []); */
 
   useEffect(() => {
     if (!loading) {
@@ -56,7 +102,7 @@ export function PersonScore() {
   }, []); */
 
   // memoize () & need not not be recreated on every render.
-  const handleReset = useCallback(() => setScore(0), []);
+  // const handleReset = useCallback(() => setScore(0), []);
 
   if (loading) {
     return <div>loading...</div>;
@@ -78,28 +124,31 @@ export function PersonScore() {
       <p>{expensiveCalculation}</p>
       <button
         ref={addButtonRef}
-        onClick={() => {
+        /*  onClick={() => {
           setScore(score + 1);
-        }}
+        }} */
+        onClick={() => dispatch({ type: 'increment' })}
       >
         add
       </button>
       <button
-        onClick={() => {
+        /*  onClick={() => {
           setScore(score - 1);
-        }}
+        }} */
+        onClick={() => dispatch({ type: 'decrement' })}
       >
         subtract
       </button>
-      {/* <button
-        onClick={() => {
-          setScore(0);
-        }}
-      >
-        reset
-      </button> */}
-
-      <Reset onProp={handleReset} />
+      {
+        <button
+          /* onClick={() => {
+            setScore(0);
+          }} */
+          onClick={() => dispatch({ type: 'reset' })}
+        >
+          reset
+        </button>
+      }
     </div>
   );
 }
